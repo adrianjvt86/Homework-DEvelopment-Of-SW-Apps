ee83d500c3cf24dcf843244cd08af67ea20a4fcd
Adrian Vasquez <adrianjvt86@gmail.com>
2017-12-05 12:28:19 +0100

    Networking Advance

---
+++ HomeworkDSA/networkmanager.cpp
@@ -0,0 +1,11 @@
+#include "networkmanager.h"
+#include <QNetworkReply>
+#include <QNetworkRequest>
+
+#include <QDir>
+#include <QFile>
+#include <QJsonArray>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QObject>
+
